13c05ad8f649b3dce051822f48168090ed0642c7
Ravi Singh <rpsingh3@ncsu.edu>
2016-03-25 17:53:48 -0400

    Fix compile issue

--- SerialRead.py
+++ SerialRead.py
@@ -1,5 +1,6 @@
 import serial
 import sys
+import struct
 
 ser_port = serial.Serial('/dev/ttyACM0',9600,timeout=1)
 first_packet = 1
@@ -9,9 +10,16 @@ f = open('output_file', 'wb')
 pa_size = 1
 message = 'A'
 
+def bytes2int(str):
+ return ord(str)
+
 def carry_around_add(a,b):
     carry = a+b
     return (carry & 0xffff) + (carry >> 16)
+
+def readByte():
+    global ser_port
+    return ser_port.read()
 
 def calChecksum():
     global ser_port
@@ -21,18 +29,20 @@ def calChecksum():
     word = 0
     """ Forming bytearray of payload by removing first 3 bytes
     1 sync/size byte and 2 checksum bytes"""
-    message = bytearray(ser_port.read())
+    #message = bytearray()
+    #message.extend(readByte())
+    message = []
     if pa_size == 4:
-        a = ser_port.read()
-        message.append(a[0])
-        a = 0x00
+        a = readByte()
+        message.append(a)
+        a = '0' 
         message.append(a[0])
     else:
         while pa_size > 4:
-            a = ser_port.read()
-            message.append(a[0])
+            a = readByte()
+            message.append(a)
             pa_size = pa_size - 1
-
+    data = message.pack("%dB" % len(data), *message)
     for i in range(0,len(message), 2):
         word = ord(message[i]) + (ord(message[i+1]) << 8)
         sum_words = carry_around_add(sum_words,word)
@@ -41,45 +51,60 @@ def calChecksum():
 def detectError():
     global ser_port
     global pa_size
-    #checksum_firstbyte = ser_port.read()		
-    #checksum_secondbyte = ser_port.read()		
+    #checksum_firstbyte = readByte()		
+    #checksum_secondbyte = readByte()		
     ''' Extracting 16 bits checksum'''
-    checksum = ser_port.read()
-    a = ser_port.read()
-    checksum.append(a[0])
+    checksum = bytearray()
+    checksum.extend(readByte())
+    a = readByte()
+    checksum.extend(a)
    
     if calChecksum() == checksum:
         return 1
-    else
+    else:
         return 0
     
 while True:
-    first_byte = ser_port.read()
+    first_byte = bytearray()
+    first_byte.extend(readByte())
+
+
+
+    print first_byte
     if first_packet == 0:
-        pa_size = first_byte & size_mask
+        pa_size = ord(first_byte) & size_mask
 
         if pa_size == 1:
             last_packet = 1
             f.close()
         else:
+            print "Checking checksum"
+    """
             if detectError() == 1:
- 	        sendNACK()
+ 	        #sendNACK()
+                print "send NACK"
 	    else:
-                sendACK()
+                #sendACK()
+                print "send ACK"
                 f.write(message)
-                
+    """              
                                  
     if first_packet == 1:
-        pa_size = first_byte & size_mask
+        pa_size = ord(first_byte) & size_mask
         if pa_size == 1:
             last_packet = 1
             f.close()
         else:
 	    while pa_size > 1:
-	        first_oct = ser_port.read()
-	        second_oct = ser_port.read()
-	    	third_oct = ser_port.read()
-	    	fourth_oct = ser_port.read()
+                first_oct = bytearray()
+	        first_oct.extend(readByte())
+                second_oct = bytearray()
+	        second_oct.extend(readByte())
+                third_oct = bytearray()
+	        third_oct.extend(readByte())
+                fourth_oct = bytearray()
+	        fourth_oct.extend(readByte())
+                serverIP = str(first_oct)+'.'+str(second_oct)+'.'+str(third_oct)+'.'+str(fourth_oct)
             	pa_size = pa_size - 4
  			
 	    first_packet = 0
