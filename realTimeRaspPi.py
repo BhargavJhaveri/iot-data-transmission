3ca5d3f9a36c9299f3af76a8216059ed21ef8629
Mahdi Inaya <mahdiinaya@gmail.com>
2016-03-26 06:07:04 +0000

    updated working code for real time file transfer RaspPi

--- realTimeRaspPi.py
+++ realTimeRaspPi.py
@@ -9,32 +9,6 @@ def GPIO_init():
     GPIO.setup(7, GPIO.OUT)
     GPIO.output(7,0)
 
-def TCPConn_inits():
-"""    try:
-        PC1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       	PC1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-	PC1.bind((TCP_IP, TCP_PORT_PC1))
-        PC1.listen(1)
-        connPC1, addrPC1 = PC1.accept()
-    except:
-        print 'TCP CONNECTION ERR - PC1'
-        e = sys.exc_info()[0]
-	print e
-	PC1.close()
-        sys.exit(1)
-
-    try:
-    PC2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    PC2.setcoketopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    PC2.bind((TCP_IP, TCP_PORT_PC2))
-    PC2.listen(1)
-    connPC2, addrPC2 = PC2.accept()
-    except:
-        print 'TCP CONNECTION ERR - PC2'
-        PC1.close()
-        PC2.close()
-        sys.exit(1)
-"""
 def sendPacket(char):
     var = 0
     charBin = string_to_bits(char)
@@ -44,7 +18,7 @@ def sendPacket(char):
     for i in range(8):
 	GPIO.output(7, int(charBin[i]))
         time.sleep(sleep)
-    	print int(charBin[i])
+  #  	print int(charBin[i])
 
 def string_to_bits(string_data):
     return ''.join(bin(ord(ch))[2:].zfill(8) for ch in string_data)
@@ -52,9 +26,9 @@ def string_to_bits(string_data):
 
 sleep = 1/100.0000
 
-TCP_IP = '10.139.61.61'
-TCP_PORT_PC1 = 5004
-TCP_PORT_PC2 = 5006
+TCP_IP = '10.139.67.91'
+TCP_PORT_PC1 = 5062
+TCP_PORT_PC2 = 5063
 BUFFER_SIZE = 1
 
 GPIO_init()
@@ -73,11 +47,42 @@ except:
     PC1.close()
     sys.exit(1)
 
+try:
+    PC2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PC2.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    PC2.bind((TCP_IP, TCP_PORT_PC2))
+    PC2.listen(1)
+    connPC2, addrPC2 = PC2.accept()
+    connPC2.settimeout(2)
+except:
+    print 'TCP CONNECTION ERR - PC2'
+    PC1.close()
+    PC2.close()
+    sys.exit(1)
+
 
 while 1: #we need to finalize the protocol
     dataToTransmit = connPC1.recv(BUFFER_SIZE)
     ack =  None
-    #while ack != dataToTransmit:
+    timeoutCount = 0
+    retransmit = 0
+    print "input:",dataToTransmit
     sendPacket(dataToTransmit)
     GPIO.output(7, 0)
-#	ack = connPC2.recv(BUFFER_SIZE)
+    try:
+         ack = connPC2.recv(BUFFER_SIZE)
+         print "received1:", ack
+    except socket.timeout:
+	 retransmit = 1
+	 print 'Socket timeout'
+         pass	
+    if ack != dataToTransmit or retransmit == 1:
+         sendPacket(dataToTransmit)
+         GPIO.output(7, 0)
+         try:
+              ack = connPC2.recv(BUFFER_SIZE)
+              print "received2:", ack
+         except socket.timeout:
+              retransmit = 0
+              print 'Socket timeout'
+              pass
