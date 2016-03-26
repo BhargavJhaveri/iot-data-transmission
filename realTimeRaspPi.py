64ae379b30a15545ce4c5dcff7e5c6ec1c376dff
Mahdi Inaya <mahdiinaya@gmail.com>
2016-03-26 02:15:20 +0000

    updated code

--- realTimeRaspPi.py
+++ realTimeRaspPi.py
@@ -2,6 +2,7 @@
 import socket
 import RPi.GPIO as GPIO
 import time
+import sys
 
 def GPIO_init():
     GPIO.setmode(GPIO.BOARD)
@@ -9,18 +10,22 @@ def GPIO_init():
     GPIO.output(7,0)
 
 def TCPConn_inits():
-    try:
+"""    try:
         PC1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        PC1.bind((TCP_IP, TCP_PORT_PC1))
+       	PC1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+	PC1.bind((TCP_IP, TCP_PORT_PC1))
         PC1.listen(1)
         connPC1, addrPC1 = PC1.accept()
     except:
         print 'TCP CONNECTION ERR - PC1'
-        PC1.close()
+        e = sys.exc_info()[0]
+	print e
+	PC1.close()
         sys.exit(1)
 
     try:
     PC2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PC2.setcoketopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     PC2.bind((TCP_IP, TCP_PORT_PC2))
     PC2.listen(1)
     connPC2, addrPC2 = PC2.accept()
@@ -29,15 +34,17 @@ def TCPConn_inits():
         PC1.close()
         PC2.close()
         sys.exit(1)
-
+"""
 def sendPacket(char):
+    var = 0
     charBin = string_to_bits(char)
+    '''print charBin'''
+    charBin = [1, charBin[1], charBin[2], charBin[3], charBin[4], charBin[5], charBin[6], charBin[7]]
+    '''print charBin'''
     for i in range(8):
-	if i == 0:
-	     GPIO.output(7, 1)
-        else:
-	     GPIO.output(7, charBin[i])
+	GPIO.output(7, int(charBin[i]))
         time.sleep(sleep)
+    	print int(charBin[i])
 
 def string_to_bits(string_data):
     return ''.join(bin(ord(ch))[2:].zfill(8) for ch in string_data)
@@ -45,37 +52,32 @@ def string_to_bits(string_data):
 
 sleep = 1/100.0000
 
-TCP_IP = ''
-TCP_PORT_PC1 = 5005
+TCP_IP = '10.139.61.61'
+TCP_PORT_PC1 = 5004
 TCP_PORT_PC2 = 5006
 BUFFER_SIZE = 1
 
-
-
 GPIO_init()
-TCPConn_inits()
+
+try:
+    PC1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PC1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    PC1.bind((TCP_IP, TCP_PORT_PC1))
+    PC1.listen(1)
+    connPC1, addrPC1 = PC1.accept()
+except:
+    print 'TCP CONNECTION ERR - PC1'
+    e = sys.exc_info()[0]
+    print e
+    PC1.shutdown(socket.SHUT_RDWR)
+    PC1.close()
+    sys.exit(1)
+
 
 while 1: #we need to finalize the protocol
-    dataToTransmit = PC1.recv(BUFFER_SIZE)
+    dataToTransmit = connPC1.recv(BUFFER_SIZE)
+    ack =  None
+    #while ack != dataToTransmit:
     sendPacket(dataToTransmit)
-    ack = PC2.recv(BUFFER_SIZE) #pc2 will return the data that it received
-    while ack != dataToTransmit:
-	sendPacket(dataToTransmit)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    GPIO.output(7, 0)
+#	ack = connPC2.recv(BUFFER_SIZE)
