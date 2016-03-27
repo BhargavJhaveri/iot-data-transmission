988e4f68bb1da01671eab7b2bbb397839541b34b
bhargavjhaveri@gmail.com <DHOOM2>
2016-03-26 22:56:06 -0400

    RPi file transfer code update.

--- fileTransmissionRaspPi.py
+++ fileTransmissionRaspPi.py
@@ -4,26 +4,33 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
+
 def GPIO_init():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
-    GPIO.output(7,0)
+    GPIO.output(7, 0)
 
+
+# Send the data in the form of light pulses.
 def sendPacket(char):
     var = 0
     charBin = string_to_bits(char)
     charBin = [1, charBin[1], charBin[2], charBin[3], charBin[4], charBin[5], charBin[6], charBin[7]]
     for i in range(8):
-	GPIO.output(7, int(charBin[i]))
+        GPIO.output(7, int(charBin[i]))
         time.sleep(sleep)
-  #  	print int(charBin[i])
+        #  	print int(charBin[i])
 
+
+# Convert the data from String to binary/bits.
 def string_to_bits(string_data):
     return ''.join(bin(ord(ch))[2:].zfill(8) for ch in string_data)
 
 
-sleep = 1/100.0000
+# Sleep time for LED.
+sleep = 1 / 100.0000
 
+# Socket Connection fields.
 TCP_IP = '10.139.64.106'
 TCP_PORT_PC1 = 6004
 TCP_PORT_PC2 = 6005
@@ -31,9 +38,10 @@ BUFFER_SIZE = 1
 
 GPIO_init()
 
+# PC1 connection.
 try:
     PC1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    PC1.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    PC1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     PC1.bind((TCP_IP, TCP_PORT_PC1))
     PC1.listen(1)
     connPC1, addrPC1 = PC1.accept()
@@ -46,9 +54,10 @@ except:
     PC1.close()
     sys.exit(1)
 
+# PC2 connection.
 try:
     PC2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    PC2.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    PC2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     PC2.bind((TCP_IP, TCP_PORT_PC2))
     PC2.listen(1)
     connPC2, addrPC2 = PC2.accept()
@@ -60,21 +69,26 @@ except:
     PC2.close()
     sys.exit(1)
 
-
 timeStart = time.time()
 byteCount = 0
 retransmissionCount = 0
 errors = 0
-while 1: #we need to finalize the protocol
+
+while 1:
+    # Receive the values from the sender.
+    # It is a blocking call.
     dataToTransmit = connPC1.recv(BUFFER_SIZE)
-    ack =  None
+
+    ack = None
+
     byteCount += 1
     timeoutCount = 0
     retransmit = 0
     currentError = 0
-    print "input:",dataToTransmit
+
+    print "input:", dataToTransmit
     if ord(dataToTransmit) != 127:
-	sendPacket(dataToTransmit)
+        sendPacket(dataToTransmit)
     else:
         sendPacket(dataToTransmit)
         timeEnd = time.time()
@@ -82,34 +96,35 @@ while 1: #we need to finalize the protocol
         print "Byte Count=", byteCount
         print "Retransmission Count =", retransmissionCount
         print "serviceTime =", serviceTime
-        print "avg speed = (bps)", byteCount/serviceTime*8
-        print "Erros:", errors
-	print "Error Rate:", errors*1.0/byteCount
+        print "avg speed = (bps)", byteCount / serviceTime * 8
+        print "Errors:", errors
+        print "Error Rate:", errors * 1.0 / byteCount
         sys.exit(1)
 
     GPIO.output(7, 0)
     try:
-         ack = connPC2.recv(BUFFER_SIZE)
-         print "received1:", ack
+        ack = connPC2.recv(BUFFER_SIZE)
+        print "received1:", ack
     except socket.timeout:
-	 retransmit = 1
-	 print 'Socket timeout'
-         pass	
-    while ack != dataToTransmit and retransmit <= 3:
+        retransmit = 1
+        print 'Socket timeout'
+        pass
 
-         retransmissionCount += 1
-         sendPacket(dataToTransmit)
-         GPIO.output(7, 0)
-         try:
-              ack = connPC2.recv(BUFFER_SIZE)
-              if ack != dataToTransmit:
-                  retransmit += 1
-              print "retransmission",retransmit
-              print "received:", ack
-         except socket.timeout:
-              retransmit += 1
-              print 'Socket timeout'
-              pass
+    # Check the ack. Compare with the transmitted bit and re-transmit accordingly.
+    while ack != dataToTransmit and retransmit <= 3:
+        # Check number of retransmission of the given character.
+        sendPacket(dataToTransmit)
+        GPIO.output(7, 0)
+        try:
+            ack = connPC2.recv(BUFFER_SIZE)
+            if ack != dataToTransmit:
+                retransmit += 1
+            print "retransmission trials", retransmit
+            print "received:", ack
+        except socket.timeout:
+            retransmit += 1
+            print 'Socket timeout'
+            pass
+    # Increase the error count of, the transmission was not successful.
     if ack != dataToTransmit:
         errors += 1
-
